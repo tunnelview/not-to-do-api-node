daeb5a7f757a04728958bc2f0790d12d7d151e3a
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-08 14:42:40 +1000

    Add REST client

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+// Task api endpoints
+app.get(`/api/v1/tasks`, (req, res) => {
+  res.json({
+    message: "This is my api code",
+  });
+});
+
 app.get("/", (req, res) => {
   res.json({
     message: "You have reached the not to api server",
