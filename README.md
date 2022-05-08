ec9e10f4ac46f6c1ac5fd95626d75fb3887597df
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-08 13:49:56 +1000

    Add description to README.md and server.js

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # not-to-do-api-node
+
+# not-to-do-api-node Server
+
+This is a api server for the react not to do list
+The repo for the react not to do list can be found here ...
+
+## API's
+
+### Task API
+
+### User API
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "You have reached the not to api server",
+  });
+});
+
 app.listen(PORT, (error) => {
   error && console.log(error);
 
